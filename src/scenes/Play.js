669ce4da5206913977d2c5ad14fd9e64e61bc9a9
Nhan <nhtrnguy@ucsc.edu>
2023-04-24 16:20:03 -0700

    fix bug

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -11,7 +11,6 @@ class Play extends Phaser.Scene {
         this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
     }
     create() {
-        this.add.text(20, 20, "Rocket Patrol Play");
         // green UI background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
         // white borders
